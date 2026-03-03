e8aa62d0d7811bf5c9425df7691272f3717309c4
Nime V R <nimevr109@gmail.com>
2026-03-03 22:58:22 +0530

    Bubble updated with Swapped boolean variable

--- Sorting_Technique/src/sort/BubbleSort.java
+++ Sorting_Technique/src/sort/BubbleSort.java
@@ -12,12 +12,17 @@ public class BubbleSort {
 		int[] arr= {13,46,24,52,20,9};
 		int n=arr.length;
 		for(int i=0;i<n;i++) {
+			boolean swapped=false;
 			for(int j=0;j<n-1-i;j++) {
 				if(arr[j]>arr[j+1]) {
 					swap(arr,j,j+1);
+					swapped=true;
 				}
 			}
-			
+			if(!swapped) {
+				break;
+			}
+			System.out.println("Array at Interation "+(i+1));
 			for(int k=0;k<n;k++) {
 				System.out.print(arr[k]+" ");
 			}
